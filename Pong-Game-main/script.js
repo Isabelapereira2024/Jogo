b78526fe45090bbea22ea10866e20176431f177f
Isabelapereira2024 <isabelasantospereira23@gmail.com>
2024-09-09 11:43:42 -0300

    Update script.js

--- Pong-Game-main/script.js
+++ Pong-Game-main/script.js
@@ -33,7 +33,7 @@ var Ai = {
             height: 180,
             x: side === 'left' ? 150 : this.canvas.width - 150,
             y: (this.canvas.height / 2) - 35,
-            score: 3,
+            score: 0,
             move: DIRECTION.IDLE,
             speed: 11
         };
